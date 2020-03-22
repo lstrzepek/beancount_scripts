591ce501be30f7be1f7edabd49078bef9b5fbb43
Łukasz Strzępek <lstrzepek@users.noreply.github.com>
2020-03-22 23:53:59 +0100

    Fixed missing same date transactions

--- bean_sort.py
+++ bean_sort.py
@@ -6,10 +6,21 @@ import re
 with open(sys.argv[1], "r", encoding="UTF-8") as file:
     buffer = {}
     index = "0000-00-00"
+    with open(sys.argv[1], "r", encoding="UTF-8") as file:
+    buffer = {}
+    tnx=0
     for line in file:
         if re.match(r'\d\d\d\d-\d\d-\d\d', line):
             index = line[:10]
-            buffer[index] = []
-        buffer[index].append(line)
+            if index not in buffer:
+                buffer[index] = []
+                tnx=0
+                buffer[index].append([line])
+            else:
+                tnx=len(buffer[index])
+                buffer[index].append([line])
+        else:
+            buffer[index][tnx].append(line)
     for i in sorted(buffer.keys()):
-        print("".join(buffer[i]))
+        for j in buffer[i]:
+            print("".join(j))
